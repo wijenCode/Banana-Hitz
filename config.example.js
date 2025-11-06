a1777cc79db95d1639220f753376d562c1f1f1af
wijenCode <izzedineelfatih@student.telkomuniversity.ac.id>
2025-11-07 02:08:44 +0700

    fix bug

--- config.example.js
+++
@@ -1,7 +0,0 @@
-// Template Configuration - Upload file ini ke GitHub
-// Copy file ini menjadi config.js dan isi dengan nomor asli
-
-const CONFIG = {
-    businessWhatsApp: '6282162885153', // Ganti dengan nomor WhatsApp bisnis Anda (format: 62xxx)
-    businessName: 'Banana Hitz'
-};
